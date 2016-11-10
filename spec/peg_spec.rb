68478b532e5ba2554401ef11477088f9e51e9017
Timothy Meixell <misternu@gmail.com>
2016-11-09 23:11:59 -0600

    Add test for peg equality method

--- spec/peg_spec.rb
+++ spec/peg_spec.rb
@@ -8,4 +8,16 @@ describe Peg do
       expect(peg.color).to eq "red"
     end
   end
+
+  describe'#==' do
+    let(:peg1) { Peg.new("yellow") }
+    let(:peg2) { Peg.new("yellow") }
+    let(:peg3) { Peg.new("orange") }
+    it 'returns false if they are not the same' do
+      expect(peg2 == peg3).to be false
+    end
+    it 'returns true if they are not the same' do
+      expect(peg1 == peg2).to be true
+    end
+  end
 end
