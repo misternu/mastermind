c1df64b5d633a5be221c3fcb51e6aea9574ba6de
Timothy Meixell <misternu@gmail.com>
2016-11-09 23:14:08 -0600

    Override peg equality method

--- peg.rb
+++ peg.rb
@@ -1,11 +1,16 @@
 class Peg
   COLORS = ["red", "green", "blue", "yellow", "brown", "orange", "black", "white"]
 
+  attr_reader :color_index
   def initialize(color)
     @color_index = COLORS.index(color)
   end
 
   def color
     COLORS[@color_index]
+  end
+
+  def ==(other_peg)
+    @color_index == other_peg.color_index
   end
 end
