e6d1a56b13ed35024f214329d94f37df581ca1dc
Timothy Meixell <misternu@gmail.com>
2016-11-09 23:08:37 -0600

    Store color as an integer

--- peg.rb
+++ peg.rb
@@ -1,6 +1,11 @@
 class Peg
-  attr_reader :color
+  COLORS = ["red", "green", "blue", "yellow", "brown", "orange", "black", "white"]
+
   def initialize(color)
-    @color = color
+    @color_index = COLORS.index(color)
+  end
+
+  def color
+    COLORS[@color_index]
   end
 end
